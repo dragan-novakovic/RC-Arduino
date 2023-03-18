858f4d03d6697738be99d6e97dd4b61cccaee5cf
dragan1810 <d.novakovic1810@yahoo.com>
2023-03-18 22:34:53 +0100

    qqq

--- uno-transmitter/src/main.rs
+++ uno-transmitter/src/main.rs
@@ -1,27 +1,26 @@
 #![no_std]
 #![no_main]
 
+use arduino_hal::prelude::*;
 use panic_halt as _;
 
 #[arduino_hal::entry]
 fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
+    let mut serial = arduino_hal::default_serial!(dp, pins, 57600);
 
-    /*
-     * For examples (and inspiration), head to
-     *
-     *     https://github.com/Rahix/avr-hal/tree/main/examples
-     *
-     * NOTE: Not all examples were ported to all boards!  There is a good chance though, that code
-     * for a different board can be adapted for yours.  The Arduino Uno currently has the most
-     * examples available.
-     */
-
-    let mut led = pins.d13.into_output();
-
+    //Controls
+    //let mut led = pins.d13.into_output();
+    let up = pins.d10.into_floating_input();
+    let down = pins.d9.into_pull_up_input();
+    ufmt::uwriteln!(&mut serial, "Got {}!\r", "Start").void_unwrap();
     loop {
-        led.toggle();
-        arduino_hal::delay_ms(1000);
+        if up.is_high() {
+            ufmt::uwriteln!(&mut serial, "Got {}!\r", "Napred").void_unwrap();
+        }
+        if down.is_high() {
+            ufmt::uwriteln!(&mut serial, "Got {}!\r", "Nazad").void_unwrap();
+        }
     }
 }

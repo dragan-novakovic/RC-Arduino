cd0c97995bb67af549db990fa4ead638ffff7d03
dragan1810 <d.novakovic1810@yahoo.com>
2023-03-21 20:32:48 +0100

    qqq

--- uno-transmitter/src/main.rs
+++ uno-transmitter/src/main.rs
@@ -11,16 +11,13 @@ fn main() -> ! {
     let mut serial = arduino_hal::default_serial!(dp, pins, 57600);
 
     //Controls
-    //Currently using pull down, until I understand pull up xD
+    //Currently using pull down, until I understand pull up...
+    // Aparently when using pull up the value is inverted, and I need to use different input
     let up = pins.d2.into_floating_input();
-    let down = pins.d4.into_floating_input();
 
     loop {
         if up.is_high() {
             ufmt::uwriteln!(&mut serial, "Got {}!\r", "Napred").void_unwrap();
-        }
-        if down.is_high() {
-            ufmt::uwriteln!(&mut serial, "Got {}!\r", "Nazad").void_unwrap();
         }
     }
 }

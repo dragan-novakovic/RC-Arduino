491fa51a8a62d34089946466635f82432ae184bf
dragan1810 <d.novakovic1810@yahoo.com>
2023-03-22 21:10:20 +0100

    qq

--- uno-transmitter/src/main.rs
+++ uno-transmitter/src/main.rs
@@ -1,7 +1,7 @@
 #![no_std]
 #![no_main]
 
-use arduino_hal::{hal::Spi, pac::SPI, prelude::*};
+use arduino_hal::{hal::Spi, pac::SPI, prelude::*, spi::Settings};
 use panic_halt as _;
 
 #[arduino_hal::entry]
@@ -11,7 +11,14 @@ fn main() -> ! {
     let mut serial = arduino_hal::default_serial!(dp, pins, 57600);
 
     //Data line
-    // let spi = Spi::new(SPI, sclk, mosi, miso, cs, settings);
+    let (mut spi, _) = Spi::new(
+        dp.SPI,
+        pins.d13.into_output(),
+        pins.d11.into_output(),
+        pins.d12.into_pull_up_input(),
+        pins.d10.into_output(),
+        Settings::default(),
+    );
 
     //Controls
     //Currently using pull down, until I understand pull up...
@@ -21,7 +28,12 @@ fn main() -> ! {
     loop {
         if up.is_high() {
             ufmt::uwriteln!(&mut serial, "Got {}!\r", "Napred").void_unwrap();
-            // Spi::send(spi, b"a");
         }
+        //send
+        nb::block!(spi.send(0b00001111)).void_unwrap();
+        // Because MISO is connected to MOSI, the read data should be the same
+        let data = nb::block!(spi.read()).void_unwrap();
+        ufmt::uwriteln!(&mut serial, "data: {}\r", data).void_unwrap();
+        arduino_hal::delay_ms(1000);
     }
 }
